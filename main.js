29a2a665ba007445e28fd454f494edae75c2caec
terencecaiga <terencecaiga@gmail.com>
2019-04-19 00:03:22 -0700

    each iteration happens on a delay

--- main.js
+++ main.js
@@ -11,21 +11,5 @@ ASSET_MANAGER.downloadAll(function () {
     GAME_ENGINE.start();
     GAME_ENGINE.addEntity(SIMULATION);
 
-        // Creating all node objects
-        var n1 = new Node(0, 0, 1, true);
-        var n2 = new Node(0, 0, 2, true);
-        var n3 = new Node(0, 0, 3, false);
-        var n4 = new Node(0, 0, 4, false);
-    
-        NODES[0] = n1;
-        NODES[1] = n2;
-        NODES[2] = n3;
-        NODES[3] = n4;
-    
-        // Creating all edge objects
-        addEdge(1, 1, n1, n3);
-        addEdge(1, 2, n1, n4);
-        addEdge(1, 1, n3, n2);
-        addEdge(1, 2, n4, n2);
         NODE_MAP = new NodeMap();
 });
